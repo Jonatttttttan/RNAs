137cf61390e1ddb4b88be021f0d64c378802c0e5
jonatttttttan <jonathan.iuliany2@gmail.com>
2025-10-05 17:35:53 -0300

    Finalizado back propagation

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ print(f'{x.shape =}, {y.shape = }')
 # O dataframe acima indica que a variável x é uma tabela com 2 colunas e 500 linhas com valores arbitrários e y é a saída booleana.
 
 plt.scatter(x[:, 0], x[:, 1], c = y, s = 50, alpha = 0.5, cmap = 'cool') # Mostra a distribuição dos dados
-#plt.show()
+plt.show()
 
 # Modelo
 # - Inicialização dos pesos e bias
@@ -40,7 +40,7 @@ class NnModel:
 
         self.W2 = np.random.randn(self.hidden_neurons, self.output_neurons) / np.sqrt(self.hidden_neurons)
         self.B2 = np.zeros((1, self.output_neurons))
-        
+
         self.model_dict = {'W1' : self.W1, 'B1' : self.B1, 'W2': self.W2, 'B2' : self.B2}
         self.z1 = 0
         self.f1 = 0
@@ -56,11 +56,10 @@ class NnModel:
         # Equação da reta  (2)
         z2 = self.f1.dot(self.W2) + self.B2
 
-        # função de ativação (2)
-        f2 = np.tanh(z2)
+
 
         # Softmax - probabilidades das classes
-        exp_values = np.exp(f2) # Exponencial e
+        exp_values = np.exp(z2) # Exponencial e
         softmax = exp_values/np.sum(exp_values, axis = 1, keepdims=True) # axis =  coluna e keepdims mantém a dimensão
         return softmax
 
@@ -75,13 +74,47 @@ class NnModel:
         return log_prob/self.y.shape[0]
 
 
-    def backpropagation(self):
-        pass
+    def backpropagation(self, softmax: np.ndarray, learning_rate: float) -> None:
+        # Atualização W2 e B2
+        delta2 = np.copy(softmax)
+        delta2[range(x.shape[0]), y] -= 1
+        dW2 = (self.f1.T).dot(delta2)
+        dB2 = np.sum(delta2, axis=0, keepdims=True)
 
-    def fit(self):
-        pass
+        # Atualização W1 e B1
+        delta1 = delta2.dot(self.W2.T)*(1-np.power(np.tanh(self.z1), 2))
+        dW1 = (x.T).dot(delta1)
+        dB1 = np.sum(delta1, axis = 0, keepdims=True)
+
+        # Atualização dos pesos e bias
+        self.W1 += - learning_rate * dW1
+        self.W2 += - learning_rate * dW2
+        self.B1 += - learning_rate * dB1
+        self.B2 += - learning_rate * dB2
+
+
+    def fit(self, epochs: int, lr: float):
+
+        for epoch in range(epochs):
+
+            outputs = self.forward(self.x)
+            loss = self.loss(outputs)
+            self.backpropagation(outputs, lr)
+
+            # Acurácia
+            predictions = np.argmax(outputs, axis = 1)
+            correct = (predictions == y).sum()
+            accuracy = correct / y.shape[0]
+
+            if int((epoch +1) % (epochs/10)) == 0:
+                print(f'Epoch: [{epoch + 1} / {epochs}] Accuracy: {accuracy:.3f} Loss: {loss.item():.5f}')
+        return predictions
+
 
 
 modelo = NnModel(x, y, 10, 2)
-softmax = modelo.forward(x)
-print("Perda " , modelo.loss(softmax))
+result = modelo.fit(500, 0.001)
+
+# Testes e resultado
+plt.scatter(x[:, 0], x[:, 1], c = result, s = 50, alpha = 0.5, cmap = 'cool')
+plt.show()
