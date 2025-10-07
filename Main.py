e96c3d4e416add2e09dd4f51b98ee6392cfe4904
jonatttttttan <jonathan.iuliany2@gmail.com>
2025-10-07 01:02:53 -0300

    Adicionando modelos 3D

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ print(f'{x.shape =}, {y.shape = }')
 # O dataframe acima indica que a variável x é uma tabela com 2 colunas e 500 linhas com valores arbitrários e y é a saída booleana.
 
 plt.scatter(x[:, 0], x[:, 1], c = y, s = 50, alpha = 0.5, cmap = 'cool') # Mostra a distribuição dos dados
-plt.show()
+#plt.show()
 
 # Modelo
 # - Inicialização dos pesos e bias
@@ -77,13 +77,13 @@ class NnModel:
     def backpropagation(self, softmax: np.ndarray, learning_rate: float) -> None:
         # Atualização W2 e B2
         delta2 = np.copy(softmax)
-        delta2[range(x.shape[0]), y] -= 1
+        delta2[range(self.x.shape[0]), self.y] -= 1
         dW2 = (self.f1.T).dot(delta2)
         dB2 = np.sum(delta2, axis=0, keepdims=True)
 
         # Atualização W1 e B1
         delta1 = delta2.dot(self.W2.T)*(1-np.power(np.tanh(self.z1), 2))
-        dW1 = (x.T).dot(delta1)
+        dW1 = (self.x.T).dot(delta1)
         dB1 = np.sum(delta1, axis = 0, keepdims=True)
 
         # Atualização dos pesos e bias
@@ -93,7 +93,16 @@ class NnModel:
         self.B2 += - learning_rate * dB2
 
 
-    def fit(self, epochs: int, lr: float):
+
+    def show_plot(self, predictions):
+        if self.x.shape[1] != 2:
+            plt.scatter(self.x[:, 0], self.x[:,1], s = 50, c = predictions, cmap='cool', alpha = 0.7)
+            plt.show()
+        elif self.x.shape[1] == 3:
+            ax = plt.axes(projection='3d')
+            ax.scatter(self.x[:, 0], self.x[:, 1], self.x[:, 2], s=40, c=y_, cmap='cool', alpha=0.7)
+
+    def fit(self, epochs: int, lr: float, show_plot: bool = False):
 
         for epoch in range(epochs):
 
@@ -103,8 +112,10 @@ class NnModel:
 
             # Acurácia
             predictions = np.argmax(outputs, axis = 1)
-            correct = (predictions == y).sum()
-            accuracy = correct / y.shape[0]
+            correct = (predictions == self.y).sum()
+            accuracy = correct / self.y.shape[0]
+            if show_plot:
+                self.show_plot(predictions)
 
             if int((epoch +1) % (epochs/10)) == 0:
                 print(f'Epoch: [{epoch + 1} / {epochs}] Accuracy: {accuracy:.3f} Loss: {loss.item():.5f}')
@@ -112,9 +123,35 @@ class NnModel:
 
 
 
-modelo = NnModel(x, y, 10, 2)
-result = modelo.fit(500, 0.001)
+#modelo = NnModel(x, y, 10, 2)
+#result = modelo.fit(500, 0.001)
 
 # Testes e resultado
-plt.scatter(x[:, 0], x[:, 1], c = result, s = 50, alpha = 0.5, cmap = 'cool')
+#plt.scatter(x[:, 0], x[:, 1], c = result, s = 50, alpha = 0.5, cmap = 'cool')
+#plt.show()
+
+# Cluster dataset
+x_, y_= datasets.make_blobs(n_samples=400, n_features=2, centers=4, random_state=10, cluster_std=0.9, shuffle=True)
+print(x_.shape)
+print(y_.shape)
+np.unique(y_, return_counts=True)
+
+
+plt.scatter(x_[:,0], x_[:,1], s = 50, c = y_, cmap = 'cool', alpha = 0.7)
 plt.show()
+
+# Teste Modelo 2
+model = NnModel(x_, y_, 8, 4)
+result = model.fit(100, 0.001)
+
+plt.scatter(x_[:,0], x_[:,1], s = 50, c = result, cmap = 'cool', alpha = 0.7)
+plt.show()
+
+# Teste Modelo 3D
+x_, y_= datasets.make_blobs(n_samples=400, n_features=3, centers=4, random_state=50, cluster_std=0.9, shuffle=True)
+ax = plt.axes(projection='3d')
+ax.scatter(x_[:,0], x_[:,1], x_[:, 2], s = 40, c = y_, cmap = 'cool', alpha = 0.7)
+plt.show()
+
+modelo_3d = NnModel(x_, y_, 8, 4)
+result = modelo_3d.fit(500, 0.001)
